703c80a58dae8f8df05719f33f99a6419266d0ec
esraayaz <essayaz@hotmail.com>
2022-05-23 04:10:57 +0200

    Input created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import logo2 from "./logo2.svg";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo2} className="App-logo" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo2} className="App-logo" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+      </div>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/esraayaz/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Esra Ayaz
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/esraayaz/dictionary-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
