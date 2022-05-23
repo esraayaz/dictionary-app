7b48ea015c18e43bcc1a17ec8b40d12aae2dad62
esraayaz <essayaz@hotmail.com>
2022-05-23 03:31:16 +0200

    Page created

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo2 from "./logo2.svg";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo2} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
